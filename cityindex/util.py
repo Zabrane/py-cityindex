e70727fadabc56675c523af1fcdada09eb283c3a
David Wilson <dw@botanicus.net>
2012-10-21 23:39:47 +0300

    Add util.py.

---
+++ cityindex/util.py
@@ -0,0 +1,40 @@
+#
+# Copyright 2012, the py-cityindex authors
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import absolute_import
+
+
+class cached_property(object):
+    """Method decorator that exposes a property that caches the functions
+    return value on first call."""
+    _default_value = object()
+
+    def __init__(self, func, name=None, doc=None):
+        self.__name__ = name or func.__name__
+        self.__module__ = func.__module__
+        self.__doc__ = doc or func.__doc__
+        self.func = func
+
+    def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+
+        value = obj.__dict__.get(self.__name__, self._default_value)
+        if value is self._default_value:
+            value = self.func(obj)
+            obj.__dict__[self.__name__] = value
+
+        return value
